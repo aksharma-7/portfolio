fc5035c5872f2c98d136c261da6551f1dbc3e2e9
aksharma-7 <52443790+aksharma-7@users.noreply.github.com>
2021-02-13 19:15:27 +0530

    cleaned app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import Particles from 'react-particles-js';
-import particlesConfig from './config/particlesConfig';
-import About from './views/About';
-import Home from './views/Home';
-import Projects from './views/Projects';
-import Skills from './views/Skills';
 
 const App = () => {
-  return (
-    <Router>
-      <div style={{ position: 'relative', overflow: 'hidden' }}>
-        <div style={{ position: 'absolute' }}>
-          <Particles height='100vh' width='100vw' params={particlesConfig} />
-        </div>
-        <Home />
-      </div>
-      <Skills />
-      <Projects />
-      <About />
-    </Router>
-  );
+  return <h1>App</h1>;
 };
 
 export default App;
